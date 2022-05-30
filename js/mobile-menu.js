50eda05fec76b63b3339349f79be24912258834e
Viacheslav-Malyi <Malyislava@gmail.com>
2022-05-30 16:30:02 +0200

    hw-08

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector('.tel-menu-open'),
     closeMenuBtn: document.querySelector('.tel-menu-close'),
     menu: document.querySelector('.tel-menu'),
+    body: document.querySelector('body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
